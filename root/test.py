151018eb248145c2fcda546facf227025507e0c7
unknown <martinlopez89.quequen@gmail.com>
2018-03-25 21:45:40 -0300

    Test fixed with 3 methods of formating

--- root/test.py
+++ root/test.py
@@ -37,22 +37,23 @@ reader = SystemVolumeDecorator(reader)
 reader = DtmfVolumeDecorator(reader)
 reader = MusicVolumeDecorator(reader)
 reader = NotificationVolumeDecorator(reader)
-reader = RingermodeDecorator(reader)# normal=0, silent=1, vibrate=2
+reader = RingermodeDecorator(reader)  # normal=0, silent=1, vibrate=2
 reader = BatteryTempDecorator(reader)
 reader = BatteryVoltageDecorator(reader)
 reader = BatteryChargerDecorator(reader)
 reader = ScreenBrightnessDecorator(reader)
-reader = ScreenModeDecorator(reader)# manual=0, automatic=1
-reader = ScreenPowerDecorator(reader)# off=0, on=1
+reader = ScreenModeDecorator(reader)  # manual=0, automatic=1
+reader = ScreenPowerDecorator(reader)  # off=0, on=1
 reader = CPUDecorator(reader)
 reader = MemoryFreeDecorator(reader)
 
-saveType = "3PerHour"
+saveType = "25"
 import sys
-index=0
+
+index = 0
 
 with open(filePath, 'rb')as inFile:
-    with open('file2_3PerHour.csv', 'wb') as outFile:
+    with open('file2_25.csv', 'wb') as outFile:
         readFile = csv.reader(inFile, delimiter=';', quotechar='|')
         writeFile = csv.writer(outFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         writeFile.writerow(reader.get_firstRow())
@@ -62,62 +63,62 @@ with open(filePath, 'rb')as inFile:
         min00 = 0
         if saveType == "24PerDay":
             for row in readFile:
-                index=index+1
+                index = index + 1
                 line = reader.read(row)
-                if line[7]!=hour:
+                if line[7] != hour:
                     hour = line[7]
                     writeFile.writerow(line)
-                sys.stdout.write("Progress: %d%%   \r" % (index*100/170204505) )
+                sys.stdout.write("Progress: %d%%   \r" % (index * 100 / 170204505))
                 sys.stdout.flush()
         elif saveType == "3PerHour":
             for row in readFile:
                 line = reader.read(row)
-                index=index+1
+                index = index + 1
+                sys.stdout.write("Progress: %d%%   \r" % (index * 100 / 170204505))
+                sys.stdout.flush()
                 if line[7] == hour:
                     if min20 == 0:
-                        if line[8]>=15 and line[8]<=25:
-                            min20=1
+                        if line[8] >= 15 and line[8] <= 25:
+                            min20 = 1
                             writeFile.writerow(line)
                     if min40 == 0:
-                        if line[8]>=35 and line[8]<=45:
-                            min40=1
+                        if line[8] >= 35 and line[8] <= 45:
+                            min40 = 1
                             writeFile.writerow(line)
                     if min00 == 0:
-                        if line[8]>=50 and line[8]<=59:
-                            min00=1
+                        if line[8] >= 50 and line[8] <= 59:
+                            min00 = 1
                             writeFile.writerow(line)
                 else:
                     hour = line[7]
-                    min00=0
-                    min20=0
-                    min40=0
-                sys.stdout.write("Progress: %d%%   \r" % (index*100/170204505) )
-                sys.stdout.flush()
+                    min00 = 0
+                    min20 = 0
+                    min40 = 0
         else:
-            index =0
+            index = 0
             for row in readFile:
-                index = index+1
+                index = index + 1
                 line = reader.read(row)
-                if index%25 == 0:
+                if index % 25 == 0:
                     if count < readFile.line_num:
                         writeFile.writerow(line)
-                sys.stdout.write("Progress: %d%%   \r" % (index/12121) )
+                sys.stdout.write("Progress: %d%%   \r" % (index / 170204505))
                 sys.stdout.flush()
 
-#import fileinput
+# import fileinput
 
-#seen = set()  # set for fast O(1) amortized lookup
-#for line in fileinput.FileInput('result.csv', inplace=1):
+# seen = set()  # set for fast O(1) amortized lookup
+# for line in fileinput.FileInput('result.csv', inplace=1):
 #    if any(line) in seen: continue  # skip duplicate
 
 #    seen.add(line)
 #    print line  # standard output is now redirected to the file
 
-#input = open('result.csv', 'rb')
-#output = open('result2.csv', 'wb')
-#writer = csv.writer(output)
-#for row in csv.reader(input):
+# input = open('result.csv', 'rb')
+# output = open('result2.csv', 'wb')
+# writer = csv.writer(output)
+# for row in csv.reader(input):
 #    if any(row):
 #        writer.writerow(row)
-#input.close()
-#output.close()
+# input.close()
+# output.close()
